d9d037448a6abdafe36d73a45d6e33ce323d05c8
ironkrush <krush2752@gmail.com>
2024-04-06 16:55:17 +0530

    code updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -92,7 +92,7 @@ function Footer() {
                 <h1>🔥</h1>
               </div>
             </div>
-            <div className="elem" id="pic3" data-image="https://images.unsplash.com/photo-1712081024208-f04e1956d736?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOHx8fGVufDB8fHx8fA%3D%3D">
+            <div className="elem" id="pic3" data-image="om.jpg">
               <div className="overlay"></div>
               <div className="emojis">
               <div className="surname">
