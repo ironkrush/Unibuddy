28fbab9b34d2dade42defe7471a60f631dbb500e
Hardik Patel <hardik.patel@bacancy.com>
2024-04-07 12:27:39 +0530

    implement pdf viewer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import FirstSem from './pages/FirstSem';
@@ -11,10 +11,45 @@ import SeventhSem from './pages/SeventhSem';
 import EightSem from './pages/EightSem';
 import Content from './components/Content';
 
+
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/TextLayer.css";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+// refrenece link : https://github.com/wojtekmaj/react-pdf/blob/main/sample/create-react-app-5/src/Sample.tsx
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  'pdfjs-dist/build/pdf.worker.min.js',
+  import.meta.url,
+).toString();
+
+const options = {
+  cMapUrl: '/cmaps/',
+  standardFontDataUrl: '/standard_fonts/',
+};
+const maxWidth = 1200;
 function App() {
+
+  const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState();
+  const [pageNumber, setPageNumber] = useState(1);
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+  function onDocumentLoadSuccess({ numPages: nextNumPages }) {
+    setNumPages(nextNumPages);
+  }
   return (
     <>
-    <Router>
+      <Document file={'https://www.adobe.com/support/products/enterprise/knowledgecenter/media/c4611_sample_explain.pdf'} onLoadSuccess={onDocumentLoadSuccess} options={options} >
+        {Array.from(new Array(numPages), (el, index) => (
+          <Page
+            key={`page_${index + 1}`}
+            pageNumber={index + 1}
+            width={maxWidth}
+          />
+        ))}
+      </Document>
+
+      {/* <Router>
       <Routes> 
         <Route path="/" element={<Home />} />
         <Route path="/firstsem" element={<FirstSem />} />
@@ -34,9 +69,9 @@ function App() {
         <Route path="/seventhsem/:id" element={<Content />} />
         <Route path="/eightsem/:id" element={<Content />} />
       </Routes>
-    </Router>
-    
-     </>
+    </Router> */}
+
+    </>
   );
 }
 
