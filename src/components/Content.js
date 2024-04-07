d5a4235c21e07f90ae205c40fe43d926c705271e
ironkrush <krush2752@gmail.com>
2024-04-08 04:33:17 +0530

    pdfs added

--- src/components/Content.js
+++ src/components/Content.js
@@ -12,13 +12,13 @@ function Content() {
 
   const subjectData = {
     1: {
-      pdfLink: "https://drive.google.com/file/d/13R8iOoJqEp7QDJGX0oipdxUla2FYmAgI/view?usp=sharing",
+      pdfLink: "/Papers/Sem1Maths.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLvDFTOvV1tapwV_9bgZk33_J_aVlNZloS&si=PgF4cmpFP5gsDGpM"
       ]
     },
     2: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem1Fop.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=video3",
         "https://www.youtube.com/watch?v=video4",
@@ -26,7 +26,7 @@ function Content() {
       ]
     },
     3: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem1Civil.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=video5",
         "https://www.youtube.com/watch?v=video6",
@@ -34,7 +34,7 @@ function Content() {
       ]
     },
     4: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem1Eg.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/playlist?list=PLDN15nk5uLiC9wFOTiDp8ekWAf40BwSct",
         "https://www.youtube.com/playlist?list=PLDN15nk5uLiBpnIOK5r3KXdfFOVzGHJSt",
@@ -43,7 +43,7 @@ function Content() {
       ]
     },
     5: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem1Fop.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=video5",
         "https://www.youtube.com/watch?v=video6",
@@ -51,7 +51,7 @@ function Content() {
       ]
     },
     6: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem1Es.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=video5",
         "https://www.youtube.com/watch?v=video6",
@@ -110,7 +110,7 @@ function Content() {
       ]
     },
     13: {
-      pdfLink: "https://drive.google.com/file/d/1TjngRzmn3GFKRISLSgosa35yv7eL7o2E/view?usp=sharing",
+      pdfLink: "/Papers/Sem3Decrete.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=ynHoB_0li_o&list=PLT3bOBUU3L9gF5nz4y1j2lb6q6IDbA6PQ",
         "https://www.youtube.com/watch?v=g5c7xKcNWr0&list=PL15h-I4HvELJSw75O2ZVe0BKEQuzWBigB",
@@ -121,19 +121,19 @@ function Content() {
       ]
     },
     14: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem3Dsa.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/playlist?list=PLu0W_9lII9ahIappRPN0MCAgtOu3lQjQi"
       ]
     },
     15: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem3De.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/playlist?list=PLBlnK6fEyqRjMH3mWf6kwqiTbT798eAOm"
       ]
     },
     16: {
-      pdfLink: "/pdf1.pdf#toolbar=0",
+      pdfLink: "/Papers/Sem3itw.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLo6pcfMH2eBr4rB1ztAFwoqBwFGbqoB-f&si=g5N4BUqplCkW_xRn",
         "https://youtube.com/playlist?list=PLo6pcfMH2eBrPql0h-h-sWR4DkrEHGa4e&si=o1yn1O86sUy_6u0d",
@@ -141,68 +141,68 @@ function Content() {
       ]
     },
     17: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem3Dbms.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=6Iu45VZGQDk&list=PLBlnK6fEyqRi_CUQ-FXxgzKQ1dwr_ZJWZ"
 
       ]
     },
     18: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem4Pom.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=vOykcERGw9Y&list=PLLy_2iUCG87DH0iQSVWZ8iamVl5SaLlXQ"
       ]
     },
     19: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem4Psnm.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=V3iEsLPAD68&list=PLU6SqdYcYsfLRq3tu-g_hvkHDcorrtcBK"
 
       ]
     },
     20: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem4Coa.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=Ol8D69VKX2k&list=PLBlnK6fEyqRgLLlzdgiTUKULKJPYc0A4q"
 
       ]
     },
     21: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem4Os.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=vBURTt97EkA&list=PLBlnK6fEyqRiVhbXDGLXDk_OQAeuVcp2O"
 
       ]
     },
     22: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem4Oopj.pdf#toolbar=0",
       youtubeLinks: [
         "https://www.youtube.com/watch?v=AEo4KgwKYoU&list=PLqleLpAMfxGCbdaJ6SoExDfHrTfRDeWeG"
       ]
     },
     23: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem5SE.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLxCzCOWd7aiEed7SKZBnC6ypFDWYLRvB2&si=L0PzSe_2YKDpHPyS"
 
       ]
     },
     24: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem5TOC.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLBlnK6fEyqRgp46KUv4ZY69yXmpwKOIev&si=z0gLRWVCDMvc2DaE"
 
       ]
     },
     25: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem5DAA.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLYwpaL_SFmcBOrMihdkd48kgs6_YP8taa&si=y2PVQN7-AFRcHlQP"
 
       ]
     },
     26: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem5CN.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLBlnK6fEyqRgMCUAG0XRw78UA8qnv6jEx&si=kJ1nkkedIC5Zg8fG"
       ]
@@ -224,27 +224,27 @@ function Content() {
       ]
     },
     29: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem5Map.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLxCzCOWd7aiHL7mF_dRsj4Q9x1NNaZqkh&si=sjxgklf2tDoAm6To"
       ]
     },
     30: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem6AI.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLxCzCOWd7aiHGhOHV-nwb0HR5US5GFKFI&si=W7bXuNqOMwGD4BYf"
 
       ]
     },
     31: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem6Python.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg&si=CECyyV61YwIb1lSb"
 
       ]
     },
     32: {
-      pdfLink: "",
+      pdfLink: "/Papers/Sem6CNS.pdf#toolbar=0",
       youtubeLinks: [
         "https://youtube.com/playlist?list=PLBlnK6fEyqRgJU3EsOYDTW7m6SUmW6kII&si=BMxmJsSOEznsXYzR"
 
@@ -346,7 +346,7 @@ function Content() {
     <div>
     
       <div id='inside'>
-      <h1 class="headcontent">Previous Year Papers</h1>
+      <h1 className="headcontent">Previous Year Papers</h1>
         <div id="pyq">
           {pdfIframe}
           
